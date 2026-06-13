8139477e46107684898f56b5d0009cfaec3f7be1
Rithvickkr <12216061@nitkkr.ac.in>
2026-06-13 18:03:07 +0530

    chore(mac): bump homebrew cask to v2.4.0 dmg sha256

--- installer/homebrew/contextvolt.rb
+++ installer/homebrew/contextvolt.rb
@@ -13,7 +13,7 @@
 
 cask "contextvolt" do
   version "2.4.0"
-  sha256 "53c4c3bb3cce2ddb46266971276015b0f7c4943b07d7bfb419bd43555c6fb291"
+  sha256 "d89640477e1c712b210396fa33b13913acc37d65024f489f8b0320fdbc0c5430"
 
   url "https://github.com/Rithvickkr/ContextVolt/releases/download/v#{version}/ContextVolt-#{version}-macOS.dmg",
       verified: "github.com/Rithvickkr/ContextVolt/"
